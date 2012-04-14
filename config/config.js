1b1d4fa91ea6d968c84c5af0ee462812ba8007ff
Rylan Bowers <rylan.bowers@Rylan Bowers.local>
2012-04-14 15:32:52 -0600

    Removing keys.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 /*
-I hate having this public! :)
 */
 exports.config = {
   //Server settings
@@ -11,7 +10,4 @@ exports.config = {
   rdio_oauth_access: 'http://api.rdio.com/oauth/access_token',
   rdio_oauth_auth: 'https://www.rdio.com/oauth/authorize?oauth_token=',
   rdio_api: 'http://api.rdio.com/1/',
-  rdio_api_key: 'bhv3ckpuwzexvtgf6gztst3h',
-  rdio_api_shared: 'sR4aDBKAgu',
-  songs_to_grab: 10,
 };
